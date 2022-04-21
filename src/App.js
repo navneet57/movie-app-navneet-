3675fd85edada45953289dada86a8dcab15be01f
navneet57 <navneetkumarjtori30@gmail.com>
2022-04-21 19:50:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
+import "@fortawesome/fontawesome-free/css/solid.css";
+
+import Home from "./components/Home";
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
